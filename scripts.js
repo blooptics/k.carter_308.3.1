d781976c8418b52f0f0bcac808fd19f93393cd3e
blooptics <kenya.carter123@gmail.com>
2025-12-04 19:47:23 -0500

    Finish the best of my ability

--- scripts.js
+++ scripts.js
@@ -4,10 +4,62 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("Fizz Buzz ");
     } else if (i % 3 == 0) {
-        console.log("Fizz -" ;
+        console.log("Fizz");
     }else if (i % 5 ==0){
         console.log("Buzz ")
     }else{
         console.log(i);
+    }
+}
+
+// Prime Time
+for (let n = 5; n < 12; n++) {
+    if (n % 1 === 0 && n % 2 === 0 && n % 3 === 0) {
+        continue;
+    }
+    console.log(n);
+}
+
+//I couldnt figure it out
+
+
+
+
+// //Feeling Loopy
+let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+let commas = 0;
+
+for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    if (current == ",") {
+        commas++;
+    } else if (current == "\n") {
+        console.log(cell1, cell2, cell3, cell4);
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+
+        commas = 0;
+    } else {
+        if (commas == 0) {
+            cell1 += current;
+        } else if (commas == 1) {
+            cell2 += current;
+        } else if (commas == 3) {
+            cell3 += current;
+        } else {
+            cell4 += current;
+        }
+    }
+
+    if (i + 1 == str.length) {
+        console.log(cell1, cell2, cell3, cell4);
     }
 }
